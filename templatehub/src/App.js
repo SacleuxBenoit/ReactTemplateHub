fe16103d2483407dcdab07837c1e7b327827133c
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-30 15:18:52 +0100

    create header (mobile only for now)

--- templatehub/src/App.js
+++ templatehub/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home';
+import MainHeader from './Components/Header/MainHeader';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <MainHeader />
         <Routes>
           <Route path="*" element={<Home />} />
         </Routes>
---
+++ templatehub/src/Components/Header/MainHeader.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import('../../Styles/Header/MainHeader.css')
+
+function MainHeader() {
+    const [displayBurgerMenu, setDisplayBurgerMenu] = useState(false)
+
+    function burgerMenu() {
+        setDisplayBurgerMenu(!displayBurgerMenu)
+    }
+    return (
+        <>
+            <div className='containerMainHeader'>
+                <Link to="/"><div>TemplateHub</div></Link>
+                <div>
+                    <svg onClick={burgerMenu} className='svgBurgerMenu' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            {displayBurgerMenu ? (
+                                <>
+                                    <path d="M3 12h18" stroke="#fff" strokeWidth="1.2" strokeLinecap="round"></path>
+                                    <path d="M3 6h18" stroke="#fff" strokeWidth="1.2" strokeLinecap="round"></path>
+                                    <path d="M3 18h18" stroke="#fff" strokeWidth="1.2" strokeLinecap="round"></path>
+                                </>
+                            ) : (
+                                <>
+                                    <path d="M20 7L4 7" stroke="#fff" strokeWidth="1.7" strokeLinecap="round"></path>
+                                    <path d="M20 12L4 12" stroke="#fff" strokeWidth="1.7" strokeLinecap="round"></path>
+                                    <path d="M20 17L4 17" stroke="#fff" strokeWidth="1.7" strokeLinecap="round"></path>
+                                </>
+                            )}
+                        </g>
+                    </svg>            </div>
+                <div className='searchBar'><input type="text" placeholder="search..." /></div>
+
+            </div>
+            {displayBurgerMenu && (
+                <div className='listMainHeader'>
+                    <ul>
+                        <Link to="/"><li>Header</li></Link>
+                        <Link to="/"><li>Footer</li></Link>
+                        <Link to="/"><li>NavBar</li></Link>
+                    </ul>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default MainHeader
