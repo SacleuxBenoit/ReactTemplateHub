c3118b886973605b97968c37b9d80f9ebde3e7c2
SacleuxBenoit <benoitsacleux@outlook.fr>
2024-01-01 21:55:26 +0100

    add first tests for the header

--- templatehub/src/Components/Header/CHjournal.js
+++ templatehub/src/Components/Header/CHjournal.js
@@ -1,7 +1,37 @@
 import '../../Styles/Header/SHjournal.css'
+import { Link } from 'react-router-dom'
 function CHjournal() {
     return (
-        <div>CHjournal</div>
+        <header>
+            <div className='journalContainer'>
+                <svg className='svgBurgerMenuJournal' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" transform="rotate(0)">
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.576"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path d="M20 7L4 7" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path>
+                        <path d="M20 12L4 12" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path>
+                        <path d="M20 17L4 17" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path>
+                    </g>
+                </svg> {/* Link that svg to navBarLeft*/}
+                <div className='headerLeft'>
+                    Journal
+                </div>
+
+                <div className='headerRight'>
+                    <Link to="/">
+                        <svg className='svgConnectionJournal' fill="#000000" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg">
+                            <title>user--online</title>
+                            <circle cx="26" cy="16" r="4"></circle>
+                            <path d="M22,30H20V25a5,5,0,0,0-5-5H9a5,5,0,0,0-5,5v5H2V25a7,7,0,0,1,7-7h6a7,7,0,0,1,7,7Z" transform="translate(0)"></path>
+                            <path d="M12,4A5,5,0,1,1,7,9a5,5,0,0,1,5-5m0-2a7,7,0,1,0,7,7A7,7,0,0,0,12,2Z" transform="translate(0)"></path>
+                            <rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" fill="none" width="32" height="32"></rect>
+                        </svg>
+                    </Link>
+                    <Link to="/">S'abonner</Link>
+                </div>
+
+            </div>
+        </header>
     )
 }
 
